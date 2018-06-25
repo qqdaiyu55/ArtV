d8b0aa1fe25bd40ab7f5bb5bdd154a47924f69c0
daiyu <qqdaiyu55@gmail.com>
2018-06-25 15:08:48 +0800

    Develop sidebar: add buttons and tree view

--- src/renderer/components/sidebar.js
+++ src/renderer/components/sidebar.js
@@ -1,17 +1,49 @@
 const React = require('react')
+const Tree = require('react-ui-tree')
 
 const { dispatcher } = require('../lib/dispatcher')
 
 class SideBar extends React.Component {
   render () {
+    const tree = {
+      "module": "react-ui-tree",
+      "children": [{
+        "collapsed": true,
+        "module": "dist",
+        "children": [{
+          "module": "node.js"
+        }]
+      }]
+    }
+
     return (
-      <div></div>
+      <div>
+        <div className='button-wrapper'>
+          <span className='add-source'></span>
+          <span className='setting'></span>
+        </div>
+        <div className='tree'>
+          <Tree
+            paddingLeft={15}
+            tree={tree}
+            renderNode={this.renderNode}
+          />
+        </div>
+      </div>
     )
   }
 
   getAddButton () {
     return (
       <button onClick={dispatcher('addSource')} />
+    )
+  }
+
+  renderNode (node) {
+    return (
+      <span>
+        {node.module}
+      </span>
     )
   }
 }
